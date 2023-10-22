d4dc6c697b0aa83809de616a4f737d5652de7ee0
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-10-22 19:25:00 -0400

    Create main 'App' with header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Head';
+
+/**
+ * Create app
+ * @return {ReactHTMLElement} App
+ */
+function App() {
+	return (
+		<>
+			<Header/>
+		</>
+	);
+}
+
+export default App;
